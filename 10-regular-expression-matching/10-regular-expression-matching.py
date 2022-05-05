10c25d9b806a96906b25a2661227b37b77f811ce
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-05 15:15:50 -0400

    Time: 64 ms (62.56%), Space: 14.2 MB (15.51%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -3,7 +3,7 @@ class Solution:
         
         
         
-#         # 150
+#         # 90 min, used lots of help
 #         # if we simply wanted to check if p matches s then we can iterate and make sure both match
 #         # if we end up oob but somehting still has more then false
 #         if len(s) != len(p):
@@ -44,10 +44,9 @@ class Solution:
             if (pdx + 1 < len(p) and p[pdx + 1] == "*"):                 # if next has a star, check possibilities
                 addChar = False
                 if sdx < len(s) and (s[sdx] == p[pdx] or p[pdx] == "."): # adding the char if current is matching
-                    addChar = dfs(sdx + 1, pdx)             
+                    addChar = dfs(sdx + 1, pdx)
                 if addChar is False:                                     # adding char doesn't work, try skipping char
                     skipChar = dfs(sdx, pdx + 2)
-                # match = match or dfs(sdx, pdx + 2)        
                 memo[(sdx, pdx)] = addChar or skipChar
                 return memo[(sdx, pdx)]
             
@@ -59,11 +58,8 @@ class Solution:
             # if next isn't a star, and current doesn't match, then return False
             return False
         
-        
         memo = {}        
         return dfs(0, 0)
-                
-            
-            
-            
+    
+    
             
