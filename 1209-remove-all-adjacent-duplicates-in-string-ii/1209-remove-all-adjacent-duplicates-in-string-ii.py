7f4cde88d54bab5f2bdb82abf53e2ddaa766426e
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-06 10:43:24 -0400

    Time: 146 ms (61.26%), Space: 18.7 MB (69.77%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -4,13 +4,41 @@ class Solution:
         
         
         
-        # 950
+#         # 40 min :(
         
-        # brute force would be to continue deleting windows of k until there are no changes, n^2 and 1
-        # we can use a stack to help us and make this N and N
-        #  store input inside stack, while current == prev, remove them if >= k
-        #  reverse stack, join it, return it
-        # stack gave tle
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ######################
+    
+    
+#         # brute force would be to continue deleting windows of k until there are no changes, n^2 and 1
+#         # we can use a stack to help us and make this N and N
+#         #  store input inside stack, while current == prev, remove them if >= k
+#         #  reverse stack, join it, return it
+#         # stack gave tle
         
 #         stack = []
 #         for char in s:
@@ -23,26 +51,28 @@ class Solution:
         
 #         return ''.join(stack)
     
+    
+#         # this still gives tle since the all() checks for everything up to -k
+#         stack = []
+#         for char in s:
+#             stack.append(char)
+#             while (len(stack) >= k and all( c == char for c in stack[-k:] )):
+#                 del stack[-k:]
+#                 # stack = stack[:len(stack) - k]
+#         return ''.join(stack)
+    
+    
+        # this passed, we stored and incremented the last character
+        # finally we recreated res and returned it
         stack = []
         for char in s:
-            # print(stack)
             if stack and stack[-1][1] == char:
                 stack[-1][0] += 1
             else:
                 stack.append([1, char])
-            # stack.append(char)
-            # print(all(c == char for c in stack[-k:]))
+
             while stack and stack[-1][0] >= k:
                 stack.pop()
-            # while (len(stack) >= k and all( c == char for c in stack[-k:] )):
-                # print(stack)
-                # del stack[-k:]
-                # stack = stack[:len(stack) - k]
-                # print(stack)
-#                 storage.append(stack.pop())
-#                 if len(storage) >= k:
-#                     storage.clear()
-#             stack.extend(storage)
+
         res = [ char[1] * char[0] for char in stack ]
         return ''.join(res)
-        return ''.join(stack)
