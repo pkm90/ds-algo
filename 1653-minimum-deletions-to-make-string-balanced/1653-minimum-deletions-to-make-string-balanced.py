af5ca825ab28b850257d8263d7f53f26300a57e4
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-10 16:15:26 -0400

    Time: 1701 ms (15.09%), Space: 19.9 MB (13.66%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,4 +1,5 @@
-# couldn't do it...
+# finished but don't fully understand solution,
+# same as https://leetcode.com/problems/flip-string-to-monotone-increasing/submissions/
 
 class Solution:
     def minimumDeletions(self, s: str) -> int:
@@ -12,64 +13,31 @@ class Solution:
         # find how many need to be flipped after current
         # return the min of sum of both
         
-        
-        prefixA, prefixB = [], [0] # [0] * (len(s) + 1), [0] * (len(s) + 1)
-        # prefixA.append(0), prefixB.append(0)
+        prefixA, prefixB = [], [0]
         countA, countB = 0, 0
         minSwaps = float(inf)
-        for i, char in enumerate(s):
+        
+        for char in s:
             if char == 'b':
                 countB += 1
-            if char == 'a':
-                countA += 1
             prefixB.append(countB)
-            # prefixA.append(countA)
-            # prefixB[i + 1] = countB
-            # prefixA[i + 1] = countA
-        countA, countB = 0, 0
-        for i, char in enumerate(s[::-1]):
+            
+        for char in s[::-1]:
             if char == 'a':
                 countA += 1
             prefixA.append(countA)
-        # prefixB.append(prefixB[-1])
-        # prefixB.reverse()
+        
         prefixA.reverse()
         prefixA.append(0)
         # print(prefixA)
         # print(prefixB)
+        
         for i in range(len(prefixA)):
             minSwaps = min(minSwaps, prefixB[i] + prefixA[i])
+        
         return minSwaps
         
-        return 0 if minSwaps is float(inf) else minSwaps
         
-        
-        
-        
-# The question is similar to 926. Flip String to Monotone Increasing.
-
-# We can apply the same logic as in the solution for question 926, create a prefix sum and find the sum of 'b' to the left of a position in the string and 'a' to the right of the position in the string, this sum would be the number of deletions required to make the string balanced.
-
-# Then find the minimum of this sum by considering every possible position in the string.
-
-# class Solution:
-#     def minimumDeletions(self, s: str) -> int:
-        prefix = [0]
-        for i in range(0,len(s)):
-            prefix.append(prefix[-1] + 1 if s[i]=='b' else prefix[-1])
-        print(prefix)
-        
-        # mini = float('inf')
-        # for i in range(len(prefix)):
-        #     val = prefix[i] + (len(s) - i) - (prefix[-1] - prefix[i])
-        #     mini=min(mini,val)
-        # return mini if mini!=float('inf') else 0
-        
-        
-        
-        
-        
-        # 210
         # tried dfs and bfs bruteforce, off by one error not gonna debug
 #         minSwaps = [float(inf)]
 #         visit = set()
