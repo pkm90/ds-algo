50f6145a05e89eac36140789b82556b3d115c0a2
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-20 16:11:21 -0400

    Time: 673 ms (50.75%), Space: 15.6 MB (94.32%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        
+        
+        
+        
+        # go through grid and make edges between servers
+        # unionfind
+        # grab rank of each parent where where number of components is > 1
+        
+        # we can also count number of computers per row and col and store inside arrays
+        # iterate through grid, and if current node has more than itself in the row and col then add him
+        
+        rows, cols = len(grid), len(grid[0])
+        totalRow = [0] * rows
+        totalCol = [0] * cols
+        res = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    totalRow[r] += 1
+                    totalCol[c] += 1
+                    
+        for r in range(rows):
+            for c in range(cols):
+                if (grid[r][c] == 1
+                    and (totalRow[r] > 1 or totalCol[c] > 1)):
+                    res += 1
+         
+        print(totalRow)
+        print(totalCol)
+        return res
