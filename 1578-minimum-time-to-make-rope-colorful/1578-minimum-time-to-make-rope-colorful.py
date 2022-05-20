6bb580036219c4f8ca97c5bdde5ca0b2db446879
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-20 15:01:25 -0400

    Time: 2167 ms (9.12%), Space: 25.4 MB (6.03%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        
+        
+        
+        # sliding window, continuously remove min cost between consecutive duplicates
+        
+        res = 0
+        colors = list(colors)
+        prev, i = 0, 1
+        while i < len(colors):
+        # for left in range(len(colors) - 1):
+            print(prev, i)
+            if colors[prev] == colors[i]:
+                small, big = prev, i
+                if neededTime[small] > neededTime[big]:
+                    small, big = big, small
+                res += neededTime[small]
+                # neededTime.pop(small)
+                # colors.pop(small)
+                prev = big
+                i = i + 1
+            else:
+                prev = i
+                i += 1
+        
+        return res
+            
