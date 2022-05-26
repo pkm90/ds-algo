2421a453d1042dc0dbd606c5c5a96e8ef1844d99
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-26 11:12:08 -0400

    Time: 494 ms (74.05%), Space: 26 MB (30.23%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,34 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        
+        
+        # can sort it and check if any elements are the same
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+        # can use hashmap and add elements
+        # can put the input into a set and check length
+        
+        #################
+        
+        # hashmap, iterate and add elements
+        # if an element exists, return True
+        # time: n
+        # space: n
+        
+#         visited = set()
+#         for i in nums:
+#             if i in visited:
+#                 return True
+#             visited.add(i)
+            
+#         return False
+    
+        # alternatively, sort and search
+        # time: nlogn
+        # space: 1 if we're modifying original array, else n
+        
+        # can heapify it and pop elements until there are no more...if any two elements are same then ...
