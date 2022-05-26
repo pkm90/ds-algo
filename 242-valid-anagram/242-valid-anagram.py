bcdfbc6e797c497a974792744332cffb3ba43a27
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-26 11:20:44 -0400

    Time: 94 ms (18.05%), Space: 14.6 MB (35.17%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        
+        # can count numbers of char, and check if the counts are the same
+        countS = Counter(s)
+        countT = Counter(t)
+        return countS == countT
+
+        
+        # can sort the inputs and check equality
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # store count of all letters in hashmap
+#         # each time a letter is used, decrement
+#         # if any value in hashmap is not 0, return False else True
+#         # time: n
+#         # space: 1, we store more for each letter but max of 26 values
+        
+#         choices = Counter(s)
+        
+#         for char in t:
+#             if char not in choices:
+#                 return False
+#             else:
+#                 choices[char] -= 1
+        
+#         for value in choices.values():
+#             if value is not 0:
+#                 return False
+#         return True
