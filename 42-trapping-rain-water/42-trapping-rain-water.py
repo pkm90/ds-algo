f2ee243194daa100e5aaa6b63487f56615280808
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-27 13:36:04 -0400

    Time: 241 ms (8.40%), Space: 16 MB (13.03%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,44 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # add nothing until the first height
+        # keep track of the max so far and add difference of this and curr until last peak
+        # do the same thing for reverse of numbers after peak
+        
+        peak = [float(-inf), -1] # [ height, idx ]
+        front = None
+        back = None
+        res = 0
+        
+        for i, h in enumerate(height):
+            if peak[0] < h:
+                peak = [h, i]
+        
+        front = height[:peak[1]]
+        back = height[peak[1]:]
+        back.reverse()
+        
+        maxSoFar = 0
+        for h in front:
+            diff = maxSoFar - h
+            if diff > 0:
+                res += diff
+            maxSoFar = max(maxSoFar, h)
+            
+        maxSoFar = 0
+        for h in back:
+            diff = maxSoFar - h
+            if diff > 0:
+                res += diff
+            maxSoFar = max(maxSoFar, h)
+        
+        return res
+        
