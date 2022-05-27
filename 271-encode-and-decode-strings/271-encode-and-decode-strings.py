a5fd3bf4399d9e64f6a7646ce6845c678bf2db0b
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-27 13:16:12 -0400

    Time: 213 ms (8.14%), Space: 14.7 MB (19.35%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -9,7 +9,110 @@
 #     def decode(self, s: str) -> [str]:
 #         """Decodes a single string to a list of strings.
 #         """
+
+
+# class Codec:
+#     def encode(self, strs: [str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
         
+
+#     def decode(self, s: str) -> [str]:
+#         """Decodes a single string to a list of strings.
+#         """
+
+
+# class Codec:
+#     def encode(self, strs: [str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
+        
+
+#     def decode(self, s: str) -> [str]:
+#         """Decodes a single string to a list of strings.
+#         """
+
+
+
+# class Codec:
+#     def encode(self, strs: [str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
+        
+
+#     def decode(self, s: str) -> [str]:
+#         """Decodes a single string to a list of strings.
+#         """
+
+
+
+# class Codec:
+#     def encode(self, strs: [str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
+        
+
+#     def decode(self, s: str) -> [str]:
+#         """Decodes a single string to a list of strings.
+#         """
+
+
+
+class Codec:
+    def encode(self, strs: [str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        res = []
+        for s in strs:
+            res.append(str(len(s)) + "#" + s)
+        
+        print(res)
+        return ''.join(res)
+        
+
+    def decode(self, s: str) -> [str]:
+        """Decodes a single string to a list of strings.
+        """
+        res = []
+        s = list(s)
+        print(s)
+        
+        i = 0
+        while i < len(s):
+            curr = s[i]
+            
+            # grab digit
+            digit = []
+            while curr.isdigit():
+                digit.append(curr)
+                i += 1
+                curr = s[i]
+            digit = int(''.join(digit))
+            
+            # append string
+            string = s[i + 1 : i + digit + 1]
+            res.append(''.join(string))
+            
+            i += digit + 1
+            
+        return res
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -42,38 +145,38 @@
     ################3
     
     
-class Codec:
-    def encode(self, strs: [str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
-        res = []
-        for s in strs:
-            res.append(str(len(s) + 1) + "#" + s)
-            # res.append(s + '1')
-        return ''.join(res)
+# class Codec:
+#     def encode(self, strs: [str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
+#         res = []
+#         for s in strs:
+#             res.append(str(len(s) + 1) + "#" + s)
+#             # res.append(s + '1')
+#         return ''.join(res)
 
-    def decode(self, s: str) -> [str]:
-        """Decodes a single string to a list of strings.
-        """
-        print(s)
-        res = []
-        s = list(s)
-        string = []
-        i = 0
-        while i < len(s):
-            num = []
-            while s[i].isdigit():
-                num.append(s[i])
-                i += 1
-            num = int(''.join(num))
+#     def decode(self, s: str) -> [str]:
+#         """Decodes a single string to a list of strings.
+#         """
+#         print(s)
+#         res = []
+#         s = list(s)
+#         string = []
+#         i = 0
+#         while i < len(s):
+#             num = []
+#             while s[i].isdigit():
+#                 num.append(s[i])
+#                 i += 1
+#             num = int(''.join(num))
             
-            res.append(s[i + 1:i + num])
-            i = i + num
+#             res.append(s[i + 1:i + num])
+#             i = i + num
 
-        for i, s in enumerate(res):
-            res[i] = ''.join(res[i])
+#         for i, s in enumerate(res):
+#             res[i] = ''.join(res[i])
             
-        return res
+#         return res
         
 
 
