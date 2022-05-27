7bc6d35b66a72115c0fe6c1264cb8a303681201e
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-05-27 13:04:20 -0400

    Time: 149 ms (33.31%), Space: 13.9 MB (82.58%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,12 +1,16 @@
-# good hash practice, do again
+# good practice, do again
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
         
-        rows = [ set() for i in range(9) ]
-        cols = [ set() for i in range(9) ]
-        squares = collections.defaultdict(set) # (row//3, col//3) : set
+        
+        
+        
+        
+        rows = [ set() for _ in range(9) ]
+        cols = [ set() for _ in range(9) ]
+        box = collections.defaultdict(set)
         
         for r in range(9):
             for c in range(9):
@@ -16,17 +20,86 @@ class Solution:
                 
                 if val in rows[r]:
                     return False
-                rows[r].add(val)
-                
                 if val in cols[c]:
                     return False
-                cols[c].add(val)
-                
-                if val in squares[(r//3, c//3)]:
+                if val in box[(r//3, c//3)]:
                     return False
-                squares[(r//3, c//3)].add(val)
-                
+                rows[r].add(val)
+                cols[c].add(val)
+                box[(r//3, c//3)].add(val)
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #############################
+        
+#         rows = [ set() for i in range(9) ]
+#         cols = [ set() for i in range(9) ]
+#         squares = collections.defaultdict(set) # (row//3, col//3) : set
+        
+#         for r in range(9):
+#             for c in range(9):
+#                 val = board[r][c]
+#                 if not val.isdigit():
+#                     continue
+                
+#                 if val in rows[r]:
+#                     return False
+#                 rows[r].add(val)
+                
+#                 if val in cols[c]:
+#                     return False
+#                 cols[c].add(val)
+                
+#                 if val in squares[(r//3, c//3)]:
+#                     return False
+#                 squares[(r//3, c//3)].add(val)
+                
+#         return True
                 
                 
                 
