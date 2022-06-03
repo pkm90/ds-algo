2e2e4085fb650f6293969599bbf51037e474bbba
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-03 09:32:14 -0400

    Time: 92 ms (7.67%), Space: 14.3 MB (88.74%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -2,9 +2,9 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         
+        #########################################
         
-        
-        
+        # using template: https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template.-solved-many-problems
         
 #         # extend into one array and search
 #         nums = []
