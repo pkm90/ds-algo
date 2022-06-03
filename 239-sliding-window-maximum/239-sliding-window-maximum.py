a7a9fc98ab7f47f0c49a82aecbbbdd422e51a531
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-03 09:50:45 -0400

    Time: 2605 ms (36.81%), Space: 30 MB (55.14%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,7 +1,37 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
-        
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        ###############
         res = []
         l, r = 0, k - 1
         stack = collections.deque() # [index, val]
@@ -18,8 +48,6 @@ class Solution:
             while stack and stack[-1][1] < nums[r]:
                 stack.pop()
             stack.append([r, nums[r]])
-            
-
             res.append(stack[0][1])
             
         return res
