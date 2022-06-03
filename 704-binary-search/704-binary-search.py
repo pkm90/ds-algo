6f9fc19ad6142368fe1039b5a7643ee87c7d5826
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-03 09:20:29 -0400

    Time: 456 ms (9.88%), Space: 15.6 MB (22.18%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -2,8 +2,8 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         
-        if len(nums) == 1:
-            return 0 if nums[0] == target else -1
+        # if len(nums) == 1:
+        #     return 0 if nums[0] == target else -1
         
         l, r = 0, len(nums)
         while l < r:
