e250683e3a234b21b653adc5eb1783a67d5a11e8
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-03 12:12:12 -0400

    Time: 67 ms (29.35%), Space: 14.2 MB (62.84%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,93 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        
+        
+        
+        
+        
+        
+        # this is just finding pivot
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] < nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        return nums[l]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # find pivot...pivot is where the left > curr < right
+#         # if mid > right, we know htat pivot is somewhere in the right side
+        
+#         l, r = 0, len(nums) - 1
+        
+#         while l < r:
+#             mid = (r + l) // 2
+#             if nums[mid] < nums[mid - 1]: return nums[mid]
+            
+#             if nums[r] < nums[mid]:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+                
+#         print(l, r)
+#         return nums[l]
+#         # return min(nums[l], nums[r])
+        
+        
+        
+        ####################33
+        
+        
+        
+        # we have to move towards the pivot
+        # since the array was sorted in ascending, that means we can use the right pointer
+        #  to determine which partition the pivot is inside
+        # if middle > right, then the pivot is for sure somewhere in the right partition
+        
+#         l, r = 0, len(nums)-1
+        
+#         # for every iteration, as long as middle is less than the right, then we bring right down to middle
+#         #  otherwise, bring left up to middle
+#         while l < r:
+#             mid = l + (r - l) // 2
+#             if nums[r] > nums[mid]: # since right > mid, right partition is sorted and we know the pivot is in left partition
+#                 r = mid
+#             else:                   # since right < mid, we know hte pivot is in hte right partition
+#                 l = mid + 1         # we do +1 here because else is checking for equality as well (since original conditional wasn't checking for equality)
+        
+#         return nums[l]
