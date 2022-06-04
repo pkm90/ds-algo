4c02f614eef8fd792013677108404b9889d55214
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-04 15:09:02 -0400

    Time: 61 ms (63.06%), Space: 14.7 MB (81.96%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,33 +1,63 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         
-        # standard binary search
-        # the left and right will cross so we return left
+        
+        
+        
+#         l, r = 0, len(nums) - 1
+#         while l < r
+        
+        
+        
+#         # standard binary search
+#         # the left and right will cross so we return left
+#         l, r = 0, len(nums) - 1
+#         while l < r:
+#             mid = (l + r) // 2
+#             # if nums[mid] == target:
+#             #     return mid
+            
+#             # if nums[mid] < target:
+#             #     l = mid + 1
+#             # else:
+#             #     r = mid - 1
+                
+#             if target < nums[mid]:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+                
+#         print(l, r)
+#         return l
+        
+    
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
-            # if nums[mid] == target:
-            #     return mid
-            
-            if nums[mid] < target:
-                l = mid + 1
-            else:
+            if target == nums[mid]:
+                return mid
+            if target < nums[mid]:
                 r = mid - 1
-        print(l, r)
+            else:
+                l = mid + 1
         return l
-        
+    
         
         # in this example we change while condition to check if mid outside
         l, r = 0, len(nums) - 1
         while l < r:
             mid = (l + r) // 2
-            if target < nums[mid]:
-                r = mid - 1
-            else:
+#             if nums[mid] == target:
+#                 return mid
+            
+            if nums[mid] < target:
                 l = mid + 1
-                
-        print(l, r)
-        if nums[l] < target:
-            return l + 1
+            else:
+                r = mid
+        mid = (l + r) // 2
+        print(l, (l + r) // 2, r)
+        
+        if nums[mid] < target:
+            return mid + 1
         else:
-            return l
+            return mid
