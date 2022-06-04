de619916d4812e8fa402756d6ceb0b7eeee88c3c
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-04 12:50:15 -0400

    Time: 119 ms (5.16%), Space: 14.9 MB (6.25%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,22 +1,35 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         
-        
+        # standard binary search
+        # the left and right will cross so we return left
         l, r = 0, len(nums) - 1
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
+            # if nums[mid] == target:
+            #     return mid
             
             if nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
-                
+        print(l, r)
+        return l
+        
+        
+        # in this example we change while condition to check if mid outside
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+#             if nums[mid] == target:
+#                 return mid
+            
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+    
         if nums[l] < target:
             return l + 1
         else:
             return l
-        print(l, r)
-        return l
-        
