c3c6224fd2a4cac9918c3649c6921e16a165329e
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-06 14:58:22 -0400

    Time: 121 ms (5.20%), Space: 14.6 MB (81.68%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -35,38 +35,43 @@ class Solution:
         
         
         
-#         # standard binary search
-#         # the left and right will cross so we return left
-#         l, r = 0, len(nums) - 1
-#         while l < r:
-#             mid = (l + r) // 2
-#             # if nums[mid] == target:
-#             #     return mid
+        # standard binary search
+        # the left and right will cross so we return left
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l + r) // 2
+            # if nums[mid] == target:
+            #     return mid
             
-#             # if nums[mid] < target:
-#             #     l = mid + 1
-#             # else:
-#             #     r = mid - 1
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid + 1
+
+            # if nums[mid] < target:
+            #     l = mid + 1
+            # else:
+            #     r = mid - 1
                 
 #             if target < nums[mid]:
 #                 r = mid - 1
 #             else:
 #                 l = mid + 1
                 
-#         print(l, r)
-#         return l
+        print(l, r)
+        return l
         
     
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-            if target < nums[mid]:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return l
+#         l, r = 0, len(nums) - 1
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if target == nums[mid]:
+#                 return mid
+#             if target < nums[mid]:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return l
     
         
 #         # in this example we change while condition to check if mid outside
