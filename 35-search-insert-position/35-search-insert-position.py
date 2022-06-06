44c4636e42042a2076eacbe32942fc2c9376b295
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-06 14:12:05 -0400

    Time: 55 ms (79.04%), Space: 14.7 MB (39.77%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -4,6 +4,25 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+        
+        
+        
+        
+        
 #         l, r = 0, len(nums) - 1
 #         while l < r
         
@@ -31,33 +50,33 @@ class Solution:
 #         return l
         
     
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-            if target < nums[mid]:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return l
+#         l, r = 0, len(nums) - 1
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if target == nums[mid]:
+#                 return mid
+#             if target < nums[mid]:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return l
     
         
-        # in this example we change while condition to check if mid outside
-        l, r = 0, len(nums) - 1
-        while l < r:
-            mid = (l + r) // 2
-#             if nums[mid] == target:
-#                 return mid
+#         # in this example we change while condition to check if mid outside
+#         l, r = 0, len(nums) - 1
+#         while l < r:
+#             mid = (l + r) // 2
+# #             if nums[mid] == target:
+# #                 return mid
             
-            if nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid
-        mid = (l + r) // 2
-        print(l, (l + r) // 2, r)
+#             if nums[mid] < target:
+#                 l = mid + 1
+#             else:
+#                 r = mid
+#         mid = (l + r) // 2
+#         print(l, (l + r) // 2, r)
         
-        if nums[mid] < target:
-            return mid + 1
-        else:
-            return mid
+#         if nums[mid] < target:
+#             return mid + 1
+#         else:
+#             return mid
