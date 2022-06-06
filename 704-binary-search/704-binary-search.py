974605094297e05d47b3db533d88874df325cf2b
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-06 14:36:39 -0400

    Time: 269 ms (69.44%), Space: 15.4 MB (71.42%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -2,15 +2,46 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ###################
+        
         # trying out the template: https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template.-solved-many-problems
         
-        l, r = 0, len(nums) # have to cover entire search space
+        l, r = 0, len(nums) - 1 # have to cover entire search space
         while l < r:
             mid = (l + r) // 2
-            if nums[mid] > target:
+            if nums[mid] >= target:
                 r = mid
             else:
                 l = mid + 1
+                
+        if nums[l] == target:
+            return l
+        return -1
         
         print(l)
         if nums[l - 1] == target: # since l is the minimal k that satisfies condition
