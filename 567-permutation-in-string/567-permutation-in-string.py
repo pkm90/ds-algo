d99fb50f9e3a065303f0f775341acf8a71f0d24f
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-07 15:08:35 -0400

    Time: 94 ms (71.14%), Space: 14 MB (68.02%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,52 +1,93 @@
+# do again...
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
         
-        req = Counter(s1)
-        contents = {}
-        n = len(s2)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         return "Nothing"
+#         print('asfd')
+        
+        ###########################
         l = 0
+        requirements = Counter(s1)
+        contents = Counter(s2[0:len(s1)])
         
-        def invalid():
-            for key, value in contents.items():
-                if key not in req or value > req[key]:
-                    return True
-            return False
-        
-        for r in range(n):
-            val = s2[r]
-            contents[val] = contents.get(val, 0) + 1
-            
-            while l <= r and invalid():
-                contents[s2[l]] -= 1
-                if contents[s2[l]] == 0:
-                    del contents[s2[l]]
-                l += 1
-            
-            # if sum(contents.values()) == sum(req.values()):
-            #     return True
-            
-            # if valid():
-            #     return True
-            
-            if sum(contents.values()) >= sum(req.values()):
-                print(contents)
-                print(req)
-                return True
+        if requirements == contents:
+            return True
+        for r in range(len(s1), len(s2), 1):
+            contents[s2[l]] -= 1
+            contents[s2[r]] += 1
+            l += 1
 
-            
-            # while l < r and :
-            #     contents[s2[l]] -= 1
-            #     l += 1
-                
-        print(contents)
-        print(req)
+            if requirements == contents:
+                return True
+        
         return False
         
         
+        #####################################
         
+#         req = Counter(s1)
+#         contents = {}
+#         n = len(s2)
+#         l = 0
         
+#         def invalid():
+#             for key, value in contents.items():
+#                 if key not in req or value > req[key]:
+#                     return True
+#             return False
         
-        
+#         for r in range(n):
+#             val = s2[r]
+#             contents[val] = contents.get(val, 0) + 1
+            
+#             while l <= r and invalid():
+#                 contents[s2[l]] -= 1
+#                 if contents[s2[l]] == 0:
+#                     del contents[s2[l]]
+#                 l += 1
+                        
+#             if sum(contents.values()) >= sum(req.values()):
+#                 print(contents)
+#                 print(req)
+#                 return True
+                
+#         print(contents)
+#         print(req)
+#         return False
         
         
