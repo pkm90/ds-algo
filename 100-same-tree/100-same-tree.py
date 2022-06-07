5598fa8e4f4b63392081236f0b3a840f32651bdf
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-07 17:39:26 -0400

    Time: 31 ms (89.10%), Space: 13.9 MB (74.08%) - LeetHub

---
+++ 100-same-tree/100-same-tree.py
@@ -0,0 +1,97 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        
+        
+        
+        
+        
+        
+        qu = collections.deque()
+        qu.append((p, q))
+        
+        while qu:
+            currP, currQ = qu.popleft()
+            if bool(currP) != bool(currQ):
+                return False
+            if not currP:
+                continue
+            
+            if currP.val != currQ.val:
+                return False
+            qu.append((currP.left, currQ.left))
+            qu.append((currP.right, currQ.right))
+            
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        return
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # left needs to be same as right
+        # get a path including null nodes, and compare
+#         qu = collections.deque()
+#         qu.append(p)
+#         leftPath, rightPath = [], []
+        
+#         while qu:
+#             curr = qu.popleft()
+#             if curr is None:
+#                 leftPath.append(None)
+#                 continue
+#             leftPath.append(curr.val)
+#             qu.append(curr.left)
+#             qu.append(curr.right)
+        
+#         qu.append(q)
+#         while qu:
+#             curr = qu.popleft()
+#             if curr is None:
+#                 rightPath.append(None)
+#                 continue
+#             rightPath.append(curr.val)
+#             qu.append(curr.left)
+#             qu.append(curr.right)
+        
+#         return leftPath == rightPath
+    
+        # recursive solution?
+        if p is None and q is None:
+            return True
+        if bool(p) != bool(q):
+            return False
+        if p.val != q.val:
+            return False
+        
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        
+        return left and right
+        
