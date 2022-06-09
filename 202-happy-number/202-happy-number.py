8b48d1ee388978663a944c90ca0fcd0132afe41d
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-09 09:31:25 -0400

    Time: 37 ms (84.71%), Space: 13.9 MB (14.03%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        
+        visit = set()
+        while n != 1:
+            if n in visit:
+                return  False
+            visit.add(n)
+            
+            total = 0
+            while n != 0:
+                digit = n % 10
+                total += digit**2
+                n = n // 10
+            n = total
+        
+        return True
