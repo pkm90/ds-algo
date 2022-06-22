c75ae4576cf236ca98a90449450508191ddc55e9
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-22 12:42:07 -0400

    Time: 98 ms (54.46%), Space: 14.8 MB (40.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -6,7 +6,12 @@ class Solution:
         
         
         # nums = [3,2,3,1,2,4,5,5,6]
-        nums.sort()
+        # nums.sort()
+        nums = [ -1 * i for i in nums ]
+        heapq.heapify(nums)
+        for _ in range(k - 1):
+            heapq.heappop(nums)
+        return nums[0] * -1
         # print(nums)
-        return nums[-k]
+        # return nums[-k]
         
