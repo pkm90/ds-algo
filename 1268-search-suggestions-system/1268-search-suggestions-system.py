1045677c711474b7f5284a534435f6b1918a1f6d
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-28 16:08:17 -0400

    Time: 586 ms (24.42%), Space: 17.1 MB (82.32%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,45 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        
+        
+        
+        # add products to a trie
+        # iterate through searchword
+        # for each character, add it to a prefix and grab all words after prefix from trie
+        # sort the results and grab the first 3 words, append to res
+        
+
+        
+        def removeProducts(prods, pref):
+            resProds = prods.copy()
+            for pdx, p in enumerate(prods):
+                for idx, c in enumerate(pref):
+                    if idx >= len(p) or p[idx] != c:
+                        # print(resProds)
+                        # print(p, p[idx], c)
+                        # print('popping ', resProds[pdx])
+                        # resProds.pop(pdx)
+                        resProds.remove(p)
+                        break
+            # print('returning ', resProds)
+            return resProds
+        
+                
+        res = []
+        pref = ""
+        
+        # alternatively, we can simply sort the products
+        products.sort()
+        
+        # iterate through searchword
+        for char in searchWord:
+            pref += char
+            
+            # if any character doesn't have the same prefix, then remove from products
+            products = removeProducts(products, pref)#.copy()
+            # print(pref, products)
+            
+            # grab first 3 words and append to res
+            res.append(products[:3])
+                        
+        return res
