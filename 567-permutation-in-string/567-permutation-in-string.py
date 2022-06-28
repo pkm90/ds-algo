a547db80bf9b3834a72345a5a0e52bf97f6f8fe2
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-06-28 15:39:31 -0400

    Time: 2431 ms (18.94%), Space: 14.1 MB (31.24%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -11,6 +11,27 @@ class Solution:
         
         
         
+        l = 0
+        requirements = Counter(s1) # hashmap containing what we need
+        contents = Counter(s2[0:len(s1)]) # hashmap
+
+        if requirements == contents:
+            return True
+        for r in range(len(s1), len(s2), 1):
+            contents = Counter(s2[r - len(s1):r])
+        #     contents[s2[l]] -= 1
+        #     contents[s2[r]] += 1
+        #     l += 1
+
+            if requirements == contents:
+                return True
+        
+        contents = Counter(s2[len(s2) - len(s1):len(s2)])
+        if requirements == contents:
+            return True
+        
+        return False
+
         
         
         
@@ -30,20 +51,11 @@ class Solution:
         
         
         
-        
-        
-        
-        
-        
-        
-        
-#         return "Nothing"
-#         print('asfd')
         
         ###########################
         l = 0
         requirements = Counter(s1)
-        contents = Counter(s2[0:len(s1)])
+        contents = Counter(s2[0:len(s1)])        
         
         if requirements == contents:
             return True
@@ -51,11 +63,27 @@ class Solution:
             contents[s2[l]] -= 1
             contents[s2[r]] += 1
             l += 1
-
+            
             if requirements == contents:
                 return True
         
         return False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
         
         #####################################
