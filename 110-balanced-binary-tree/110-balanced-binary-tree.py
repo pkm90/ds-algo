f28bb86714504d9ffbe89d74394b04820917b0f4
pkm90 <61259692+pkm90@users.noreply.github.com>
2022-07-12 15:52:44 -0400

    Time: 94 ms (33.05%), Space: 18.7 MB (61.49%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -11,23 +11,48 @@ class Solution:
         
         
         
-#         def balanced(root):
-#             if self.res is False:
-#                 return float(inf)
-#             if root is None:
-#                 return 0
-            
-#             left = balanced(root.left)
-#             right = balanced(root.right)
-#             if abs(left - right) > 1:
-#                 self.res = False
-#             return max(left, right) + 1
+#     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
-#         self.res = True
-#         balanced(root)
-#         return self.res
+        
+        def balanced(root):
+            # if self.res is False:
+            #     return float(inf)
+            if root is None:
+                return 0
+            
+            left = balanced(root.left)
+            right = balanced(root.right)
+            if abs(left - right) > 1:
+                self.res = False
+            return max(left, right) + 1
+        
+        self.res = True
+        balanced(root)
+        return self.res
     
       
+        
+        
+        
+        
         def balanced(root):
             if self.res is False:
                 return [False, 0]
@@ -48,58 +73,23 @@ class Solution:
         return self.res
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         ####################
         
         
-#         # recursive
-#         # base: if root is None, return 0
-#         # if difference of left and right > 1, return False
-#         # return True
-#         # trying bottom up recursion, it's tricky to do this without a helper function
+        # recursive
+        # base: if root is None, return 0
+        # if difference of left and right > 1, return False
+        # return True
+        # trying bottom up recursion, it's tricky to do this without a helper function
         
-#         if root is None:
-#             return (True, 0)
-#         if root is False:
-#             return False
+        if root is None:
+            return (True, 0)
+        if root is False:
+            return False
         
-#         left = self.isBalanced(root.left) 
-#         right = self.isBalanced(root.right)
-#         if left and right and abs(left[1] - right[1]) <= 1:
-#             return (True, max(left[1], right[1]) + 1)
-#         else:
-#             return False
+        left = self.isBalanced(root.left) 
+        right = self.isBalanced(root.right)
+        if left and right and abs(left[1] - right[1]) <= 1:
+            return (True, max(left[1], right[1]) + 1)
+        else:
+            return False
