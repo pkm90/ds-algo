6e56417fb7c5f2d92f0979862978e0a2dd9a39bd
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-01 11:09:51 -0500

    Time: 246 ms (71.12%), Space: 15.4 MB (61.49%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        # continue while left <= right
+        # start at mid and update the midpoint every iteration
+        # / is normal div, returns float
+        # // is floor div, returns int, cuts off everything after decimal
+        
+        
+        
+        left = 0
+        right = len(nums) - 1
+        
+        # below works since it runs at most n times
+        # useful for debugging as it removes infinite loops
+        # while left <= right:
+        for i in range(len(nums)):
+            print(left, right)
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
