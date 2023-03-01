3368b7b54358eec1308b520bf0eebc80270ddd45
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-01 16:25:42 -0500

    Time: 3387 ms (21.67%), Space: 19.6 MB (6.71%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,42 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        
+        # return all triplets that sum==0 and are unique
+        
+        used = set()
+        res = []
+        
+        nums.sort()
+        print(nums)
+#         [     c   l        r   ]
+#         [-2, -1, -1, 0, 1, 2, 3]
+
+        
+        # if sum is < 0, iterate m
+        # if sum is > 0, iterate r
+        # if sum == 0,   iterate l
+        
+        
+        for curr in range(len(nums) - 2):
+            l = curr + 1
+            r = len(nums) - 1
+            # print(curr)
+            while l < r:
+                
+                total = nums[curr] + nums[l] + nums[r]
+
+                if total == 0:
+                    if (nums[curr], nums[l], nums[r]) not in used:
+                        print(curr, l, r)
+                        used.add((nums[curr], nums[l], nums[r]))
+                        res.append([nums[curr], nums[l], nums[r]])
+                    # else:
+                    #     break
+                if total < 0:
+                    l = l + 1
+                else:
+                    r = r - 1
+        
+        print(used)
+        return res
