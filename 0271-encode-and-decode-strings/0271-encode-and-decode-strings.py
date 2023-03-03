11cd0f4a33d27706e285e7c0a2c2fcc239bb0886
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-03 11:00:34 -0500

    Time: 81 ms (29.66%), Space: 14.5 MB (18.00%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -3,13 +3,40 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         
-        return strs
+        res = []
+        
+        for s in strs:
+            temp = str(len(s)) + "#"
+            res.append(temp + s)
+        
+        print(res)
+        res = "".join(res)
+        print(res)
+        return res
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        
-        return s
+            
+        res = []
+        idx = 0
+        while idx < len(s):
+            temp = []
+            strlen = ""
+            
+            # grab len of string
+            while s[idx] != "#":
+                strlen += s[idx]
+                idx += 1
+                
+            # grab string
+            strlen = int(strlen)
+            strEnd = idx + strlen + 1
+            res.append(s[idx + 1:strEnd])
+            idx = strEnd
+
+        print(res)
+        return res
         
 
 
