6cea82fdad60fe02205a8d6a051a35fa4c427046
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-17 17:56:07 -0400

    Time: 9412 ms (5.02%), Space: 258.4 MB (5.08%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,6 +1,60 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         
+        
+#         #most recent try, timed out
+#         if len(points) == 1:
+#             return 0
+#         res = 0
+#         visit = set()
+#         edgeCost = {}
+#         frontier = []
+        
+        
+#         adj = {}
+#         # point: [[cost, point], [cost, point]...]
+        
+#         for edge0 in points:
+#             for edge1 in points:
+#                 if edge0 == edge1:
+#                     continue
+#                 point0 = str(edge0[0]) + str(edge0[1])
+#                 point1 = str(edge1[0]) + str(edge1[1])
+#                 cost = abs(edge0[0] - edge1[0]) + abs(edge0[1] - edge1[1])
+                
+#                 if point0 in edgeCost:
+#                     edgeCost[point0].append([cost, point1])
+#                 else:
+#                     edgeCost[point0] = [[cost, point1]]
+                
+#                 if point1 in edgeCost:
+#                     edgeCost[point1].append([cost, point0])
+#                 else:
+#                     edgeCost[point1] = [[cost, point0]]
+                
+#         point = str(points[0][0]) + str(points[0][1])
+#         visit.add(point)
+
+#         for edge in edgeCost[point]:
+#             heappush(frontier, [edge[0], edge[1]])
+        
+#         while len(visit) < len(edgeCost):
+#             curr = heappop(frontier)
+#             print(curr)
+#             while curr[1] in visit:
+#                 curr = heappop(frontier)
+#             visit.add(curr[1])
+#             res += curr[0]
+            
+#             for edge in edgeCost[curr[1]]:
+#                 heappush(frontier, edge)
+        
+#         return res
+            
+            
+            
+        
+        # old solution from notes
         visited, cost = set(), 0
         frontier = [[0, (points[0])], ]
         heapq.heapify(frontier) 
@@ -29,57 +83,6 @@ class Solution:
         return cost
         
         
-        if len(points) == 1:
-            return 0
-        res = 0
-        visit = set()
-        edgeCost = {}
-        frontier = []
         
         
-        adj = {}
-        # point: [[cost, point], [cost, point]...]
         
-        for edge0 in points:
-            for edge1 in points:
-                if edge0 == edge1:
-                    continue
-                point0 = str(edge0[0]) + str(edge0[1])
-                point1 = str(edge1[0]) + str(edge1[1])
-                cost = abs(edge0[0] - edge1[0]) + abs(edge0[1] - edge1[1])
-                
-                if point0 in edgeCost:
-                    edgeCost[point0].append([cost, point1])
-                else:
-                    edgeCost[point0] = [[cost, point1]]
-                
-                if point1 in edgeCost:
-                    edgeCost[point1].append([cost, point0])
-                else:
-                    edgeCost[point1] = [[cost, point0]]
-                
-        point = str(points[0][0]) + str(points[0][1])
-        visit.add(point)
-
-        for edge in edgeCost[point]:
-            heappush(frontier, [edge[0], edge[1]])
-        
-        while len(visit) < len(edgeCost):
-            curr = heappop(frontier)
-            print(curr)
-            while curr[1] in visit:
-                curr = heappop(frontier)
-            visit.add(curr[1])
-            res += curr[0]
-            
-            for edge in edgeCost[curr[1]]:
-                heappush(frontier, edge)
-        
-        return res
-            
-            
-            
-            
-            
-            
-            
