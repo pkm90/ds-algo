e03af85fca3af8b0f7ca283f4d958552e6cae36d
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-17 10:13:39 -0400

    Time: 619 ms (78.18%), Space: 27.2 MB (78.75%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -8,19 +8,11 @@ class Solution:
         res = 0
         l = 0
         total = sum(arr[0:k])
-        # print(arr[0:k])
-        # print(len(arr))
         for r in range(k, len(arr)):
-            # 0 : 3 + 1
-            # 0 : 4
-            # 0,1,2,3
-            # print(len(arr[l:r + 1]))
-            # print(arr[l:r])
-            # print(r)
+
             if (total / k) >= threshold:
                 res += 1
             
-            # print(r)
             total = total - arr[l] + arr[r]
             l += 1
             
@@ -28,7 +20,5 @@ class Solution:
                 res += 1
                 
         return res
-        
 
-        # k * n
-            
+    
