f87a9e23bceafc3fb3c636a1d807d60e10cff421
pkm90 <61259692+pkm90@users.noreply.github.com>
2023-03-16 20:37:58 -0400

    Time: 111 ms (33.35%), Space: 15.5 MB (53.88%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -2,14 +2,16 @@ class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         
             
+            
+            
         # captain = { i:i for i in range(n) }
         captain = {}
         for i in range(n):
             captain[i] = i
         
         army = { i:1 for i in range(n) }
-        print("captains: ", captain)
-        print("army:     ", army)
+        # print("captains: ", captain)
+        # print("army:     ", army)
         
         # if root is itself, return it
         # otherwise, recursively set root to it's parent
@@ -18,14 +20,14 @@ class Solution:
                 return n
             captain[n] = find(captain[n])
             return captain[n]
-        
-        # find returns the captain of a group
-            
+                    
         def union(n1, n2):
+            # if captains of both are the are the same then do nothing
             left, right = find(n1), find(n2)
             if left == right:
                 return
             
+            # set captain to the larger group, combine groups
             if army[left] >= army[right]:
                 captain[right] = left
                 army[left] += army[right]
@@ -33,20 +35,18 @@ class Solution:
                 captain[left] = right
                 army[right] += army[left]
                 
-                
-        # edges.sort()
-        # print(edges)
+        
         for edge in edges:
             union(edge[0], edge[1])
         
         for node in range(n):
             find(node)
+
         
         print('--------------')
         print(captain)
         print(army)
-        captains = captain.values()
-        numCaptains = set(captains)
-        return len(numCaptains)
+        captains = set(captain.values())
+        return len(captains)
 
         
